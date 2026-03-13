938401dc77b8e7d2743bdb65bd5570cf7cc75e26
jvalle-2003 <josuevalle962@gmail.com>
2026-03-13 00:58:12 -0600

    fix: Quite console.log

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,6 @@
 const { Sequelize } = require("sequelize");
 require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
 
-console.log("📌 DB_SERVER desde .env:", process.env.DB_SERVER);
-console.log("📌 DB_USER desde .env:", process.env.DB_USER);
-console.log("📌 DB_PASSWORD desde .env:", process.env.DB_PASSWORD);
-console.log("📌 DB_DATABASE desde .env:", process.env.DB_DATABASE);
-
 const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USER,
